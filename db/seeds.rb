3a458af0218ab4177cecafdd751b23592572fc08
Svetlana Ivanova <svetlana.zh.ivanova@izpitai.me>
2016-07-05 18:46:05 +0300

    The begining

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do
+  Recipe.create! name: Faker::Commerce.product_name,
+                 description: Faker::Lorem.paragraphs(3, true).join("\n\n")
+end
